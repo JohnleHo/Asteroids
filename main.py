61fda7d3871136f1a39df05cfe7dd591e7614630
Johnle Ho <johnleho.dev@gmail.com>
2024-12-04 20:41:56 -0600

    Implemented bullet class and basic shooting mechanics.

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class Player(object):
         self.rotated_rect.center = (self.x, self.y)
         self.cos = math.cos(math.radians(self.angle + 90))
         self.sin = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cos + self.width // 2, self.y - self.sin * self.height // 2)
+        self.head = (self.x + self.cos * self.width // 2, self.y - self.sin * self.height // 2)
 
     def turn_right(self):
         self.angle -= 5
@@ -55,7 +55,7 @@ class Player(object):
         self.rotated_rect.center = (self.x, self.y)
         self.cos = math.cos(math.radians(self.angle + 90))
         self.sin = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cos + self.width // 2, self.y - self.sin * self.height // 2)
+        self.head = (self.x + self.cos * self.width // 2, self.y - self.sin * self.height // 2)
 
     def move_forward(self):
         self.x += self.cos * 6
@@ -65,18 +65,53 @@ class Player(object):
         self.rotated_rect.center = (self.x, self.y)
         self.cos = math.cos(math.radians(self.angle + 90))
         self.sin = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cos + self.width // 2, self.y - self.sin * self.height // 2)        
+        self.head = (self.x + self.cos * self.width // 2, self.y - self.sin * self.height // 2)        
 
+class Bullet(object):
+    def __init__(self):
+        self.point = player.head
+        self.x, self.y = self.point
+        self.width = 4
+        self.height = 4
+
+        self.cos = player.cos
+        self.sin = player.sin
+
+        self.x_velocity = self.cos * 10
+        self.y_velocity = self.sin * 10
+
+    def move(self):
+        self.x += self.x_velocity
+        self.y -= self.y_velocity
+
+    def check_offscreen(self):
+        if self.x < -50 or self.x > screen_width or self.y > self.height or self.y < -50:
+            return True
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 255), [self.x, self.y, self.width, self.height])
+    
 def redrawGameWindow():
     window.blit(bg, (0,0))
     player.draw(window)
+    
+    for bullet in player_bullets:
+        bullet.draw(window)
+
     pygame.display.update()
 
 player = Player()
+player_bullets = []
+
 running = True
 while running:
     clock.tick(60)
     if not game_over:
+        for bullet in player_bullets:
+            bullet.move()
+            if bullet.check_offscreen():
+                player_bullets.pop(player_bullets.index(bullet))
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             player.turn_left()
@@ -88,6 +123,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if not game_over:
+                    player_bullets.append(Bullet())
 
     redrawGameWindow()
 pygame.quit()
